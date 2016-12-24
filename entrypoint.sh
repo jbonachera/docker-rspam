83c2b7dc8a29dc928b01159f020eab08fc2d3532
Julien BONACHERA <julien@bonachera.fr>
2016-12-24 14:18:53 +0100

    use REDIS_HOST variable to populate config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 
 echo "password = \"$PASSWORD\";" > /etc/rspamd/local.d/worker-controller.inc
 echo "bind_socket = "*:11334";" >> /etc/rspamd/local.d/worker-controller.inc
+if [ -n "$REDIS_HOST" ]; then
+    echo servers = "$REDIS_HOST"\; >> /etc/rspamd/local.d/redis.conf
+fi
 exec /usr/bin/rspamd -c /etc/rspamd/rspamd.systemd.conf -f
